5be99a024d66e98db117cbd81cf084809b2908bb
kim.i_qschool <igorkim200218@gmail.com>
2021-11-09 16:24:12 +0300

    196479 / Создание страницы списка новостей - Исправление ошибок 2

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -7,8 +7,6 @@
 @endsection
 
 @section('info')
-    <div class="col-span-4 sm:col-span-3 lg:col-span-4 p-4">
-        <h1 class="text-black text-3xl font-bold mb-4">Новости</h1>
         <div class="space-y-4">
             @foreach($articles as $article)
                 <x-panels.articlesItem :article="$article"/>
@@ -17,6 +15,4 @@
             <x-panels.navigation.pagination/>
             </div>
         </div>
-    </div>
-    </div>
 @endsection
